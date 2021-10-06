f84106784a9b8511d88eaaafb2188c27381712b1
czhang27 <czhang27@trip.com>
2021-10-06 20:59:44 +0800

    1

--- chapter1/src/main/java/nia/chapter1/ConnectHandler.java
+++ chapter1/src/main/java/nia/chapter1/ConnectHandler.java
@@ -9,6 +9,11 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * Listing 1.2 ChannelHandler triggered by a callback
  */
 public class ConnectHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 回调的示例, 当一个新的连接被建立的时候, channelActive方法会被调用
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx)
             throws Exception {
